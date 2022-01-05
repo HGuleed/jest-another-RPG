0411687c633a729dfb8bc57c8ea4f21d2f4b1d6f
Hodan <you@example.com>
2022-01-05 06:15:44 -0500

    started game logic

--- __test__/Player.test.js
+++ __test__/Player.test.js
@@ -1,20 +1,3 @@
-const Potion = require("../lib/Potion");
-
-jest.mock("../lib/Potion");
-
-const Player = require("../lib/Player");
-
-test("creates a player object", () => {
-  const player = new Player("Dave");
-
-  expect(player.name).toBe("Dave");
-  expect(player.health).toEqual(expect.any(Number));
-  expect(player.strength).toEqual(expect.any(Number));
-  expect(player.agility).toEqual(expect.any(Number));
-  expect(player.inventory).toEqual(
-    expect.arrayContaining([expect.any(Object)])
-  );
-});
 const Player = require("../lib/Player.js");
 const Potion = require("../lib/Potion.js");
 
